fd1701edae4e28566b0de8635e577885f4a816b8
Guilherme <gsilva12401321@gmail.com>
2026-03-09 17:38:21 -0300

    Nova versão com bug de posicionamento do card na primeira coluna corrigido, e botão de opções (não está funcional até o momento) adicionado aos cards templates

--- src/javascript/kanban.js
+++ src/javascript/kanban.js
@@ -29,10 +29,155 @@ document.querySelectorAll('.kanban-cards').forEach(column => {
         e.currentTarget.classList.remove('cards-hover')
     })
 
-    column.addEventListener('drop', e => { 
-        e.currentTarget.classList.remove('cards-hover') 
+    //DROPAR O CARD NA COLUNA
+
+    column.addEventListener('drop', e => {
+        e.currentTarget.classList.remove('cards-hover')
 
         const dragCard = document.querySelector('.kanban-card.dragging');
         e.currentTarget.appendChild(dragCard);
     })
 })
+
+//FUNCAO PARA ADICIONAR CARDS
+
+document.querySelectorAll('.add-card').forEach(button => button.addEventListener('click', function () {
+
+    const column = this.closest('.kanban-column').querySelector('.kanban-cards');
+    modalNovaTarefa(column);
+
+    const addCard = document.createElement('div');
+    addCard.classList.add('kanban-card');
+    addCard.setAttribute('draggable', 'true');
+
+
+    const dragCard = document.querySelector('.kanban-card.dragging');
+
+
+}))
+
+function modalNovaTarefa(colunaSelecionada) {
+    const modal = document.createElement('div');
+    modal.classList.add('modalNovaTarefa');
+
+    const buttonDiv = document.createElement('div');
+    buttonDiv.classList.add('fecharBotaoDiv');
+
+    const modalContent = document.createElement('div');
+    modalContent.classList.add('modalContent');
+
+    const modalTexto = document.createElement('p');
+    modalTexto.innerText = 'Criar nova tarefa';
+    modalTexto.classList.add('texto-modal');
+
+    const nomeTarefa = document.createElement('input');
+
+    const selectBadge = document.createElement('select');
+
+    //OPTIONS
+
+    const optionHigh = document.createElement('option');
+    optionHigh.value = 'high';
+    optionHigh.textContent = 'Alta';
+
+    const optionMedium = document.createElement('option');
+    optionMedium.value = 'medium';
+    optionMedium.textContent = 'Média';
+
+    const optionLow = document.createElement('option');
+    optionLow.value = 'low';
+    optionLow.textContent = 'Baixa';
+
+    selectBadge.append(optionHigh, optionMedium, optionLow);
+
+    //
+
+    const labelTarefa = document.createElement('label');
+    labelTarefa.innerText = 'Nome da tarefa';
+
+    const labelSelect = document.createElement('label');
+    labelSelect.innerText = 'Prioridade';
+
+    const fecharModal = document.createElement('button');
+    fecharModal.classList.add('fecharBotao');
+
+    const buttonDiv2 = document.createElement('div');
+    buttonDiv2.classList.add('buttonDiv');
+
+    //BOTAO SALVAR TAREFA
+
+    function aplicarDrag(card) {
+
+        card.addEventListener('dragstart', e => {
+            card.classList.add('dragging')
+        })
+
+        card.addEventListener('dragend', e => {
+            card.classList.remove('dragging')
+        })
+
+    }
+
+
+    const saveTarefa = document.createElement('button');
+    saveTarefa.innerText = 'Criar Tarefa'
+    saveTarefa.classList.add('saveTarefa')
+
+    saveTarefa.addEventListener('click', function () {
+
+        const titulo = nomeTarefa.value
+
+        const prioridadeClasse = selectBadge.value
+        const prioridadeTexto = selectBadge.options[selectBadge.selectedIndex].text
+
+        if (!titulo) {
+            alert("Digite um nome para a tarefa")
+            return
+        }
+
+        const card = document.createElement('div')
+        card.classList.add('kanban-card')
+        card.setAttribute('draggable', 'true')
+
+        card.innerHTML = `
+        <div class="badge ${prioridadeClasse}">
+            <span>Prioridade ${prioridadeTexto}</span>
+        </div>
+
+        <p class="card-title">${titulo}</p>
+    `
+
+        aplicarDrag(card)
+
+        colunaSelecionada.appendChild(card)
+
+        modal.remove()
+    })
+
+    //
+
+
+    //funcao de salvar tarefa a ser criada
+
+    fecharModal.addEventListener('click', function () { modal.remove() })
+
+    const fecharIcon = document.createElement('i');
+    fecharIcon.className = "fa-solid fa-xmark";
+
+    fecharModal.appendChild(fecharIcon);
+    buttonDiv.appendChild(fecharModal);
+    modalContent.appendChild(buttonDiv);
+    modalContent.appendChild(modalTexto);
+
+    modalContent.appendChild(labelTarefa);
+    modalContent.appendChild(nomeTarefa);
+    modalContent.appendChild(labelSelect);
+    modalContent.appendChild(selectBadge);
+
+    modalContent.append(buttonDiv2);
+    buttonDiv2.append(saveTarefa);
+
+    modal.appendChild(modalContent);
+    document.body.appendChild(modal);
+
+}
